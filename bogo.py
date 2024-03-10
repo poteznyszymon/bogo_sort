bd689e8e3d94355672995da5e97f968c5ff3a538
poteznyszymon <77408864+poteznyszymon@users.noreply.github.com>
2024-03-10 16:28:56 +0100

    Update bogo.py

--- bogo.py
+++ bogo.py
@@ -1,8 +1,10 @@
-import random 
+import random
+import matplotlib.pyplot as plt
+
 
 def bogo_sort(numbers):
     number_of_shuffles = 0
-    original_numbers = numbers
+    original_numbers = numbers.copy()
     sorted_numbers = sorted(original_numbers)
     
     while original_numbers != sorted_numbers:
@@ -11,10 +13,21 @@ def bogo_sort(numbers):
     
     return number_of_shuffles
 
+
 n = 6
 min_number = 1
 max_number = 10
 numbers = [random.randint(min_number, max_number) for _ in range(n)]
 
-print(bogo_sort(numbers))
+#print(bogo_sort(numbers))
 
+times_of_shuffle = []
+times = 11
+for i in range(times):
+    times_of_shuffle.append(bogo_sort(numbers))
+print(times_of_shuffle)
+
+plt.plot(times_of_shuffle,linestyle="",marker="o")
+plt.ylabel('number of shuffles')
+plt.xlabel(f'number of sorting: {times}')
+plt.show()
